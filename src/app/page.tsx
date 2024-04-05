67d7725ef9f2bc11d74d71c8a4c2091c3e8e5848
alefdav <alefdav@gmail.com>
2024-04-05 01:01:43 -0300

    create page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,10 @@
-import Image from "next/image";
-
 export default function Home() {
-  const callAPI = async () => {
-		try {
-			const res = await fetch(
-				`https://jsonplaceholder.typicode.com/posts/1`
-			);
-			const data = await res.json();
-			console.log(data);
-		} catch (err) {
-			console.log(err);
-		}
-	};
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
         <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Consulte seu CEP</h1>
 
-        <input onInput={callAPI} type="text" id="large-input" className="block w-50 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
+        <input type="text" id="large-input" className="block w-50 p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-base focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"/>
     </main>
   );
 }
